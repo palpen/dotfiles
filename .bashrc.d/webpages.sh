37709c6946cdd0459423cb8651c31778aa5f2346
Palermo Penano <palermo.penano@mail.utoronto.ca>
2016-12-29 14:53:22 -0500

    update ball url

--- .bashrc.d/webpages.sh
+++ .bashrc.d/webpages.sh
@@ -10,7 +10,7 @@ alias seminars='open -a safari https://www.economics.utoronto.ca/index.php/index
 alias mygithub='open -a safari https://github.com/palpen;echo "Opening palermo penano github page"'
 
 # ball -- basketball schedule
-alias ball='open -a safari http://physical.utoronto.ca/FitnessAndRecreation/Drop_In_Programs/Schedules.aspx;echo "Opening basketball schedule"'
+alias ball='open -a safari https://kpe.utoronto.ca/sport-recreation/sport-and-rec-programs-z/basketball/drop-basketball;echo "Opening basketball schedule"'
 
 # csvkit tutorial
 alias csvkitt='open -a safari https://csvkit.readthedocs.org/en/latest/tutorial.html;echo "csvkit tutorial"'
