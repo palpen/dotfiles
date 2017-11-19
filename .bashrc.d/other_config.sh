3858e6835969bd882bf36f7dac597b8f5ae3646f
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-11-19 17:12:15 -0500

    virtualwrapper set up

--- .bashrc.d/other_config.sh
+++ .bashrc.d/other_config.sh
@@ -24,3 +24,19 @@ export PATH=$PATH:Library/TeX/texbin
 
 # mysql
 export PATH=$PATH:/usr/local/mysql/bin
+
+# virtual environment set up
+# reference: http://mkelsey.com/2013/04/30/how-i-setup-virtualenv-and-virtualenvwrapper-on-my-mac/
+
+export WORKON_HOME=$HOME/.virtualenvs  # ensure all new environments are isolated from the site-packages directory
+
+export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'  # use the same directory for virtualenvs as virtualenvwrapper
+
+export PIP_VIRTUALENV_BASE=$WORKON_HOM  # makes pip detect an active virtualenv and install to it
+
+export PIP_RESPECT_VIRTUALENV=true
+if [[ -r /Users/palermospenano/anaconda3/bin/virtualenvwrapper.sh ]]; then
+    source /Users/palermospenano/anaconda3/bin/virtualenvwrapper.sh
+else
+    echo "WARNING: Can't find virtualenvwrapper.sh"
+fi
